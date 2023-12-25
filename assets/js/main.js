06c38277636829eec01edd5d515d4912fe04b7b0
Romet.piir <148501967+Rometpiir@users.noreply.github.com>
2023-12-25 19:35:24 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -162,36 +162,31 @@ window.addEventListener("scroll", scrollUpfunc);
 // DARK/LIGHT THEME
 const themeButton = document.getElementById("theme-button");
 const darkTheme = "dark-theme";
-const iconTheme = "uil-sun";
+const lightTheme = "light-theme";
+const iconTheme = "uil-moon";
 
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem("selected-theme");
-const selectedIcon = localStorage.getItem("selected-icon");
+// Initially, the body has the dark theme
+document.body.classList.add(darkTheme);
 
-// obtain the current theme
-const getCurrentTheme = () =>
-  document.body.classList.contains(darkTheme) ? "dark" : "light";
-const getCurrentIcon = () =>
-  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
-
-if (selectedTheme) {
-  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
-    darkTheme
-  );
-  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
-    iconTheme
-  );
-}
+// Show dark theme icon
+themeButton.classList.add(iconTheme);
 
 // Activate/Deactivate the theme manually with the button
 themeButton.addEventListener("click", () => {
-  // Add or remove the dark icon/theme
-  document.body.classList.toggle(darkTheme);
-  themeButton.classList.toggle(iconTheme);
+  // Toggle between dark and light themes
+  if (document.body.classList.contains(darkTheme)) {
+    document.body.classList.remove(darkTheme);
+    themeButton.classList.remove(iconTheme); // Remove moon icon
+  } else {
+    document.body.classList.add(darkTheme);
+    themeButton.classList.add(iconTheme); // Add moon icon
+  }
+});
+
   // We save the theme and the current icon that the user chose
   localStorage.setItem("selected-theme", getCurrentTheme());
   localStorage.setItem("selected-icon", getCurrentIcon());
-});
+ ;
 
 // Typing Animation using Typed JS
 var typed = new Typed(".type", {
